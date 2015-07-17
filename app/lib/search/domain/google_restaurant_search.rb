2ee7ae0edea6002086d4c64338c14a651316b348
Jorg Jenni <jorg.jenni@jennius.co.uk>
2015-07-17 08:40:58 +0100

    GoogleRestaurantSearch implemented

--- app/lib/search/domain/google_restaurant_search.rb
+++ app/lib/search/domain/google_restaurant_search.rb
@@ -1,15 +1,48 @@
 module Search
   module Domain
     class GoogleRestaurantSearch
+      class << self
+        def google_max_search_radius
+          50000
+        end
+      end
+
       def initialize(google_radar_search)
         @search = google_radar_search
       end
 
       def find_places(cuisine, occasion, coordinate, radius)
         types = OccasionToGoogleTypeMapper.map(occasion)
+        places = @search.find_places(cuisine: cuisine, types: types, coordinate: coordinate, radius: radius, min_price: 0, max_price: 4)
 
-        @search.find_places(cuisine: cuisine, types: types, coordinate: coordinate, radius: radius, min_price: 0, max_price: 4).map do |place|
-          Place.new(place.placeId, place.location, 0.89)
+=begin Following calculations assigns a relevance. The first places in the result-sets are more relevant.
+     Furthermore it accounts for the fact that when using a greater radius results seem to become less
+     specific therefore less relevant.
+
+     minRelevance = radius * n + 1
+     0            = radius * n + 1 ¦ radius == GOOGLE_MAX_SEARCH_RADIUS
+     n            = -1 / GOOGLE_MAX_SEARCH_RADIUS
+     minRelevance = (radius * -1 / GOOGLE_MAX_SEARCH_RADIUS) + 1
+=end
+        min_relevance = (-radius.to_f / self.class.google_max_search_radius) + 1
+        if places.length > 1
+          n = (min_relevance-1) / (places.length-1) # for most irrelevant place (last one)
+        else
+          n = 0
+        end
+
+
+=begin
+    Following we calculate a linear function that assign relevance 1 to the first
+    and relevance minRelevance to the last element
+
+    relevance(place) = n*index(place) + 1
+    n = (relevance(place) - 1) / index(place)
+=end
+
+
+        places.each_with_index.map do |place, i|
+          Place.new(place.placeId, place.location, (n*i)+1)
         end
       end
     end
--- spec/lib/search/domain/google_restaurant_search_spec.rb
+++ spec/lib/search/domain/google_restaurant_search_spec.rb
@@ -1,40 +1,108 @@
 require 'rails_helper'
 
+GooglePlace = Search::Infrastructure::GooglePlace
+Coordinate = Search::Domain::Coordinate
+
+module Helper
+  def google_place(place_id, latitude, longitude)
+    GooglePlace.new(place_id, coordinate(latitude, longitude))
+  end
+
+  def coordinate(latitude, longitude)
+    Coordinate.new(latitude, longitude)
+  end
+end
+
+RSpec.configure do |c|
+  c.include Helper
+end
+
 module Search::Domain
   describe 'GoogleRestaurantSearch' do
-    let(:london) { Search::Domain::Coordinate.new(51.507571, -0.127702) }
-    let(:search) { Search::Domain::GoogleRestaurantSearch.new(radar_search) }
+    let(:london) { coordinate(51.507571, -0.127702) }
+    let(:search) { GoogleRestaurantSearch.new(radar_search) }
+    let(:radius) { 10000 }
+    let(:find_places) { search.find_places 'Indian food', 'lunch', london, radius }
+    let(:three_google_places) { [
+        google_place('A45', 51.004587, 0.2254),
+        google_place('B22', 51.4456, 0.7778),
+        google_place('C44', 51.2211, 0.25548)
+    ] }
 
     describe 'find_places' do
       context 'with radar_search-double' do
         let(:radar_search) { double() }
 
         it 'should return places from google' do
-          place_location = Search::Domain::Coordinate.new(51.004587, 0.2254)
-          allow(radar_search).to receive(:find_places).and_return([Search::Infrastructure::GooglePlace.new('A45', place_location)])
+          allow(radar_search).to receive(:find_places).and_return([google_place('A45', 51.004587, 0.2254)])
 
-          places = search.find_places 'Indian food', 'lunch', london, 10000
+          places = find_places
 
           expect(places).not_to be_empty
           place = places[0]
           expect(place.placeId).to be == 'A45'
-          expect(place.location).to be == place_location
+          expect(place.location).to be == coordinate(51.004587, 0.2254)
         end
 
-        it 'should calculate relevance of place'
+        context 'when radius equal 0' do
+          let(:radius) { 1 }
+
+          it 'should calculate cuisineRelevance 1 for all places' do
+            allow(radar_search).to receive(:find_places).and_return(three_google_places)
+            places = find_places
+            expect(places[0].cuisineRelevance).to be_within(0.01).of(1)
+            expect(places[1].cuisineRelevance).to be_within(0.01).of(1)
+            expect(places[2].cuisineRelevance).to be_within(0.01).of(1)
+          end
+        end
+
+        context 'when radius equal half max.radius' do
+          let(:radius) { Search::Domain::GoogleRestaurantSearch.google_max_search_radius / 2 }
+
+          it 'should calculate a decreasing cuisineRelevance for more irrelevant place' do
+            allow(radar_search).to receive(:find_places).and_return(three_google_places)
+            places = find_places
+            expect(places[0].cuisineRelevance).to be == 1
+            expect(places[1].cuisineRelevance).to be == 0.75
+            expect(places[2].cuisineRelevance).to be == 0.5
+          end
+        end
+
+        context 'when radius equal max.radius' do
+          let(:radius) { Search::Domain::GoogleRestaurantSearch.google_max_search_radius }
+
+          it 'should calculate a decreasing cuisineRelevance for more irrelevant place' do
+            allow(radar_search).to receive(:find_places).and_return(three_google_places)
+            places = find_places
+            expect(places[0].cuisineRelevance).to be == 1
+            expect(places[1].cuisineRelevance).to be == 0.5
+            expect(places[2].cuisineRelevance).to be == 0
+          end
+        end
+
+        [0, Search::Domain::GoogleRestaurantSearch.google_max_search_radius/2, Search::Domain::GoogleRestaurantSearch.google_max_search_radius].each do |r|
+          context "with radius #{r}" do
+            it 'should calculate cuisineRelevance 1 when there is only one place' do
+              allow(radar_search).to receive(:find_places).and_return([google_place('A45', 51.004587, 0.2254)])
+
+              places = find_places
+              expect(places[0].cuisineRelevance).to be == 1
+            end
+          end
+        end
       end
 
       context 'with radar_search-spy' do
         let(:radar_search) { spy() }
 
         it 'should use correct parameters' do
-
-          search.find_places 'Indian food', 'lunch', london, 10000
+          allow(radar_search).to receive(:find_places).and_return([])
+          find_places
           expect(radar_search).to have_received(:find_places).with({
                                                                        cuisine: 'Indian food',
                                                                        types: OccasionToGoogleTypeMapper.type_for_lunch,
                                                                        coordinate: london,
-                                                                       radius: 10000,
+                                                                       radius: radius,
                                                                        min_price: 0,
                                                                        max_price: 4})
         end
